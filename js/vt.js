886fa646f89c2edf0affa992b047227bf40046ee
Valentin Tsatskin <val@valtsatskin.com>
2012-04-17 23:36:59 -0400

    wip

---
+++ js/vt.js
@@ -0,0 +1,19 @@
+$(function(){
+  // Content navigation
+  var content = $('#content') // Container of goodness
+  content.find('div:not(.active)').hide()
+  $('.nav-items a').click(function(){
+    var $this = $(this)
+      , selector    = $this.attr('href')
+      , activated   = content.find(selector)
+      , active      = activated.siblings('.active')
+      , selected    = $('.nav-items a.selected')
+
+    $this.addClass('selected')
+    selected.removeClass('selected')
+    active.fadeOut(function(){
+      active.removeClass('active')
+      activated.addClass('active').fadeIn()
+    })
+  })
+})
